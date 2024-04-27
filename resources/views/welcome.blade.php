cf0ee65634b8c9e907b3dfdb509c606610b0fd35
erick <muriithierick3758@gmail.com>
2024-04-27 07:46:56 +0300

    added the functionality of deleting and mark task as complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,68 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
 </head>
+
 <body class="bg-gray-200 p-4">
- <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl">
-<h1 class="font-bold text-5xl text-center mb-8">
-    Laravel + Tailwindcss
+    <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl">
+        <h1 class="font-bold text-5xl text-center mb-8">
+            TodoApp
 
-</h1>
-<div class="mb-6">
-    <form class="flex flex-col space-y-4" method="POST" action="/">
-        <input type="text" name="title" placeholder="Todo Title" class="py-3 px-4 bg-gray-100 rounded-xl">
+        </h1>
+        <div class="mb-6">
+            <form class="flex flex-col space-y-4" method="POST" action="/">
+                @csrf
+                <input type="text" name="title" placeholder="Todo Title" class="py-3 px-4 bg-gray-100 rounded-xl">
 
-        <textarea name="description"placeholder="description" class="py-3 px-4 bg-gray-100 rounded-xl" ></textarea>
+                <textarea name="description"placeholder="description" class="py-3 px-4 bg-gray-100 rounded-xl"></textarea>
 
-        <button class="w-28 py-4 px-8 bg-green-500 text-white rounded-xl">Add</button>
-    </form>
-
-</div>
-<div class="mt-2">
-    <div class="py-4 flex items-center border-b border-gray-300 px-3">
-        <div class="flex-1 pr-8">
-            <h3 class="text-lg font-semibold">
-                The Title
-
-            </h3>
-            <p class="text-gray-500">The description</p>
+                <button class="w-28 py-4 px-8 bg-green-500 text-white rounded-xl">Add</button>
+            </form>
 
         </div>
-<div class="flex space-x-3">
-    {{-- add heroicons here --}}
-    <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
-        check
+        <div class="mt-2">
+            @foreach ($todos as $todo)
+                <div @class([
+                    'py-4 flex items-center border-b border-gray-300 px-3',
+                    $todo->completed ? bg - green - 200 : ' ',
+                ])>
+                    <div class="flex-1 pr-8">
+                        <h3 class="text-lg font-semibold">
+                            {{ $todo->title }}
 
-    </button>
-    <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
-        delete
+                        </h3>
+                        <p class="text-gray-500">{{ $todo->description }}</p>
 
-    </button>
-    
-</div>
+                    </div>
+                    <div class="flex space-x-3">
+                        <form method="POST" action="/{{ $todo->id }}">
+                            @csrf
+                            @method('PATCH')
+                            {{-- add heroicons here --}}
+                            <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
+                                check
+
+                            </button>
+                        </form>
+                        <form method="POST" action="/{{ $todo->id }}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
+                                delete
+
+                            </button>
+                        </form>
+
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
     </div>
-</div>
- </div>
 </body>
+
 </html>
