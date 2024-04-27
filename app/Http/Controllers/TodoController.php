d80bb485f20ac3260c8eff431d1fd21dfc35425d
erick <muriithierick3758@gmail.com>
2024-04-27 07:48:24 +0300

    add the update,store,delete rest methods

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+
+    public function index(){
+        $todos = Todo::all();
+        return view('welcome',[
+            'todos'=>$todos
+        ]);
+    }
+
+    public function store(){
+       
+       $data= request()->validate([
+            'title'=> 'required',
+            'description'=>'nullable'
+
+        ]);
+        Todo::create($data);
+        return redirect('/');
+    }
+    
+    public function update(Todo $todo){
+        $todo->update(['completed'=>true]);
+        return redirect('/');
+        }
+
+        public function destroy(Todo $todo){
+            $todo->delete();
+        }
 }
